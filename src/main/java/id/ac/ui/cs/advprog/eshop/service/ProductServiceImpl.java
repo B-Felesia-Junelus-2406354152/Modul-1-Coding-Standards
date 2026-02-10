2db6d8a758415698246e273dbc1efc008435f547
felesiaj <felesiaj26@gmail.com>
2026-02-10 13:47:01 +0700

    feat: add delete method in ProductService and ProductServiceImpl

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -38,4 +38,9 @@ public class ProductServiceImpl implements ProductService {
     public void edit(Product product) {
         productRepository.edit(product);
     }
+
+    @Override
+    public void delete(String productId) {
+        productRepository.delete(productId);
+    }
 }
