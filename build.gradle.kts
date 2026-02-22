e27a74862157bd39a9c168f452470d756b899c40
felesiaj <felesiaj26@gmail.com>
2026-02-22 20:23:04 +0700

    ci: integrate sonarcloud for code quality analysis

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     jacoco
     id("org.springframework.boot") version "4.0.2"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.sonarqube") version "4.4.1.3373"
 }
 
 val seleniumJavaVersion = "4.14.1"
@@ -83,4 +84,12 @@ tasks.test{
 
 tasks.jacocoTestReport {
     dependsOn(tasks.test)
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "B-Felesia-Junelus-2406354152_Modul-1-Coding-Standards")
+        property("sonar.organization", "b-felesia-junelus-2406354152")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
